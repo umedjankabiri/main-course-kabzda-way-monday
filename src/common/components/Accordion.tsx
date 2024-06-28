24f1cf4618e1750b4fea7ad8041073c37e7b7b92
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-06-28 14:58:12 +0300

    add Accordion.tsx component

---
+++ src/common/components/Accordion.tsx
@@ -0,0 +1,11 @@
+export const Accordion = () => {
+    return (
+        <div>
+            <h3>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </h3>
+        </div>
+    );
+};
