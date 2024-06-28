50138d38618865310f1c459759af6f60a15536b9
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-06-28 20:14:58 +0300

    add and changed props, changed  AppTitle.tsx to PageTitle.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,25 @@
 import './App.css'
 import {Rating} from "common/components/Rating.tsx";
 import {Accordion} from "common/components/Accordion/Accordion.tsx";
-import {AppTitle} from "../app/AppTitle.tsx";
+import {PageTitle} from "../app/PageTitle.tsx";
 
 function App() {
   console.log("App rendering")
 
   return (
     <div>
-        <AppTitle/>
+        <PageTitle title={'This is APP component'}/>
+        <PageTitle title={'My friends'}/>
         Article 1
-      <Rating/>
-      <Accordion/>
+      <Rating value={3}/>
+      <Accordion title={'Menu'}/>
         Article 2
-      <Rating/>
+      <Rating value={0}/>
+      <Rating value={1}/>
+      <Rating value={2}/>
+      <Rating value={3}/>
+      <Rating value={4}/>
+      <Rating value={5}/>
     </div>
   )
 }
