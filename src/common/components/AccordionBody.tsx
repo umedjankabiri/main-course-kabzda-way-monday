f64fc902bb6aaefff3cf98cc2a0d3cf6c6af7645
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-06-28 16:26:20 +0300

    add AccordionBody.tsx component

---
+++ src/common/components/AccordionBody.tsx
@@ -0,0 +1,13 @@
+export const AccordionBody = () => {
+    console.log("AccordionBody rendering")
+
+    return (
+        <>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </>
+    );
+};
