b05c812551e2f234470ceb8f6c9af784655d3be4
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-07-19 06:18:25 +0300

    added new component OnOff.tsx

--- src/common/components/App/App.tsx
+++ src/common/components/App/App.tsx
@@ -2,27 +2,29 @@ import 'common/components/App/App.css'
 import {Rating} from "common/components/Rating/Rating.tsx";
 import {Accordion} from "common/components/Accordion/Accordion.tsx";
 import {PageTitle} from "common/components/App/PageTitle.tsx";
+import {OnOff} from "common/components/OnOff/OnOff.tsx";
 
 function App() {
-  console.log("App rendering")
+    console.log("App rendering")
 
-  return (
-    <div>
-        <PageTitle title={'This is APP component'}/>
-        <PageTitle title={'My friends'}/>
-        Article 1
-      <Rating value={3}/>
-      <Accordion title={'Menu'} collapsed={false}/>
-      <Accordion title={'Users'} collapsed={false}/>
-        Article 2
-      <Rating value={0}/>
-      <Rating value={1}/>
-      <Rating value={2}/>
-      <Rating value={3}/>
-      <Rating value={4}/>
-      <Rating value={5}/>
-    </div>
-  )
+    return (
+        <div>
+            <PageTitle title={'This is APP component'}/>
+            <PageTitle title={'My friends'}/>
+            Article 1
+            <Rating value={3}/>
+            <Accordion title={'Menu'} collapsed={false}/>
+            <Accordion title={'Users'} collapsed={false}/>
+            Article 2
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+            <OnOff onOff={true}/>
+        </div>
+    )
 }
 
 export default App
