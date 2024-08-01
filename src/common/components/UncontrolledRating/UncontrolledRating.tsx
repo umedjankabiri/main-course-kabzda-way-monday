d321c688517059b20a7f366bd71c168c04b97cbc
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-01 17:09:11 +0300

    deleted buttons added onClick to Star.tsx component

--- src/common/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/common/components/UncontrolledRating/UncontrolledRating.tsx
@@ -7,16 +7,11 @@ export const UncontrolledRating = () => {
 
     return (
         <div>
-            <Star selected={value >= 0}/>
-            <button onClick={()=> setValue(0)}>0</button>
-            <Star selected={value >= 1}/>
-            <button onClick={()=> setValue(1)}>1</button>
-            <Star selected={value >= 2}/>
-            <button onClick={()=> setValue(2)}>2</button>
-            <Star selected={value >= 3}/>
-            <button onClick={()=> setValue(3)}>3</button>
-            <Star selected={value >= 4}/>
-            <button onClick={()=> setValue(4)}>4</button>
+            <Star selected={value > 0} onClick={()=> setValue(1)}/>
+            <Star selected={value > 1} onClick={()=> setValue(2)}/>
+            <Star selected={value > 2} onClick={()=> setValue(3)}/>
+            <Star selected={value > 3} onClick={()=> setValue(4)}/>
+            <Star selected={value > 4} onClick={()=> setValue(5)}/>
         </div>
     );
 };
