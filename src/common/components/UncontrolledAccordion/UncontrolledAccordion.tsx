8821b1ee6a183408af69694eda88cc41f414ccbb
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-01 15:59:37 +0300

    created UncontrolledAccordion.tsx component, and implemented code, created local state for controlling component from inside

---
+++ src/common/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -0,0 +1,17 @@
+import {useState} from "react";
+import {AccordionTitle} from "common/components/Accordion/AccordionTitle.tsx";
+import {AccordionBody} from "common/components/Accordion/AccordionBody.tsx";
+import {UncontrolledAccordionProps} from "common/types/UncontrolledAccordionProps.ts";
+
+export const UncontrolledAccordion = (props: UncontrolledAccordionProps) => {
+    console.log("Accordion rendering")
+    const [collapsed, setCollapsed] = useState(false);
+
+    return (
+            <>
+                <AccordionTitle title = {props.title}/>
+                <button onClick={()=> setCollapsed(!collapsed)}>toggle</button>
+                { collapsed && <AccordionBody/>}
+            </>
+    );
+};
