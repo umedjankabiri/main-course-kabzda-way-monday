91fa6e6168a7203cda899e7ddfb66aaf67f08214
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-01 16:00:23 +0300

    renamed properties of props

--- src/common/components/OnOff/OnOff.tsx
+++ src/common/components/OnOff/OnOff.tsx
@@ -5,13 +5,13 @@ export const OnOff = (props: OnOffProps) => {
 
     return (
         <div className={s.mainWrapper}>
-            <div style={props.onOff ? {backgroundColor: "green"} : {backgroundColor: "white"}} className={s.on}>
+            <div style={props.on ? {backgroundColor: "green"} : {backgroundColor: "white"}} className={s.on}>
                 ON
             </div>
-            <div style={!props.onOff ? {backgroundColor: "red"} : {backgroundColor: "white"}} className={s.off}>
+            <div style={!props.on ? {backgroundColor: "red"} : {backgroundColor: "white"}} className={s.off}>
                 OFF
             </div>
-            <div style={props.onOff ? {backgroundColor: "green"} : {backgroundColor: "red"}} className={s.circle}>
+            <div style={props.on ? {backgroundColor: "green"} : {backgroundColor: "red"}} className={s.circle}>
 
             </div>
         </div>
