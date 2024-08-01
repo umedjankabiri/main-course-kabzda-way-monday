18c4b49c1b8a1d45aac048a390766ebbac6cb47f
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-01 16:39:35 +0300

    added span with comment

--- src/common/components/OnOff/OnOff.tsx
+++ src/common/components/OnOff/OnOff.tsx
@@ -14,6 +14,7 @@ export const OnOff = (props: OnOffProps) => {
             <div style={props.on ? {backgroundColor: "green"} : {backgroundColor: "red"}} className={s.circle}>
 
             </div>
+            <span>Controlled OnOff component</span>
         </div>
     );
 };
