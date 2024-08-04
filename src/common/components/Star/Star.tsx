423bedbbd828750b000c3bd68814032fc7e6f8db
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-04 15:27:07 +0300

    refactor: added onClickHandler callback function

--- src/common/components/Star/Star.tsx
+++ src/common/components/Star/Star.tsx
@@ -2,9 +2,10 @@ import {StarProps} from "common/types/StarProps.ts";
 
 export const Star = (props: StarProps) => {
     console.log("Star rendering")
+    const onClickHandler = () => props.onClick && props.onClick(props.value)
 
     return (
-        <span onClick={()=> props.onClick && props.onClick(props.value)}>
+        <span onClick={onClickHandler}>
             {props.selected ? <b>Star </b> : "Star "}
         </span>
     )
