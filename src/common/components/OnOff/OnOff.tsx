b59b5f069640620b826127f8ffba62be7c265ad6
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-04 15:35:42 +0300

    Enhanced OnOff component added onClickOnHandler and onClickOffHandler callback function with arguments

--- src/common/components/OnOff/OnOff.tsx
+++ src/common/components/OnOff/OnOff.tsx
@@ -2,13 +2,15 @@ import {OnOffProps} from "common/types/OnOffProps.ts";
 import s from "common/components/OnOff/onOff.module.css"
 
 export const OnOff = (props: OnOffProps) => {
+    const onClickOnHandler = () => props.onClick && props.onClick(true)
+    const onClickOffHandler = () => props.onClick && props.onClick(false)
 
     return (
         <div className={s.mainWrapper}>
-            <div style={props.on ? {backgroundColor: "green"} : {backgroundColor: "white"}} className={s.on}>
+            <div style={props.on ? {backgroundColor: "green"} : {backgroundColor: "white"}} className={s.on} onClick={onClickOnHandler}>
                 ON
             </div>
-            <div style={!props.on ? {backgroundColor: "red"} : {backgroundColor: "white"}} className={s.off}>
+            <div style={!props.on ? {backgroundColor: "red"} : {backgroundColor: "white"}} className={s.off} onClick={onClickOffHandler}>
                 OFF
             </div>
             <div style={props.on ? {backgroundColor: "green"} : {backgroundColor: "red"}} className={s.circle}>
