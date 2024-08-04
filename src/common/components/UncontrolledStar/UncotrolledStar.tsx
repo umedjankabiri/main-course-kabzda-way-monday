4f6ea3171dddd8fb32cd908c3ba7dce7bd34556c
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-04 13:27:25 +0300

    Created new UncontrolledStar.tsx component, implemented code

---
+++ src/common/components/UncontrolledStar/UncotrolledStar.tsx
@@ -0,0 +1,12 @@
+import {UncontrolledStarProps} from "common/types/UncontrolledStarProps.ts";
+
+export const UncontrolledStar = (props: UncontrolledStarProps) => {
+    console.log("UncontrolledStar rendering")
+    const onClickHandler = ()=> props.onClick && props.onClick(props.value)
+
+    return (
+        <span onClick={onClickHandler}>
+            {props.selected ? <b>Star </b> : "Star "}
+        </span>
+    )
+};
