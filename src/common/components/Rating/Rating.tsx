e0d43be3e785dd5697f58b97ecec6a6557019915
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-04 14:01:34 +0300

    refactor: added onClick and value props to Rating.tsx

--- src/common/components/Rating/Rating.tsx
+++ src/common/components/Rating/Rating.tsx
@@ -6,11 +6,11 @@ export const Rating = (props: RatingProps) => {
 
     return (
         <div>
-            <Star selected={props.value > 0}/>
-            <Star selected={props.value > 1}/>
-            <Star selected={props.value > 2}/>
-            <Star selected={props.value > 3}/>
-            <Star selected={props.value > 4}/>
+            <Star selected={props.value > 0} onClick={props.onClick} value={1}/>
+            <Star selected={props.value > 1} onClick={props.onClick} value={2}/>
+            <Star selected={props.value > 2} onClick={props.onClick} value={3}/>
+            <Star selected={props.value > 3} onClick={props.onClick} value={4}/>
+            <Star selected={props.value > 4} onClick={props.onClick} value={5}/>
         </div>
     );
 };
