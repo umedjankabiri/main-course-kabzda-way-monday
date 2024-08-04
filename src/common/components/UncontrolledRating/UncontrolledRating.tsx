c538ecaf548f47ed4106494f8d512beb16ca9ef2
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-04 13:30:42 +0300

    Refactor code, deleted Star.tsx component, added UncontrolledStar.tsx component

--- src/common/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/common/components/UncontrolledRating/UncontrolledRating.tsx
@@ -1,17 +1,17 @@
-import {Star} from "common/components/Star/Star.tsx";
 import {useState} from "react";
+import {UncontrolledStar} from "common/components/UncontrolledStar/UncotrolledStar.tsx";
 
 export const UncontrolledRating = () => {
-    console.log("Rating rendering")
-    const [value, setValue] = useState<number>(0);
+    console.log("UncontrolledRating rendering")
+    const [value, setValue] = useState(0);
 
     return (
         <div>
-            <Star selected={value > 0} onClick={()=> setValue(1)}/>
-            <Star selected={value > 1} onClick={()=> setValue(2)}/>
-            <Star selected={value > 2} onClick={()=> setValue(3)}/>
-            <Star selected={value > 3} onClick={()=> setValue(4)}/>
-            <Star selected={value > 4} onClick={()=> setValue(5)}/>
+            <UncontrolledStar selected={value > 0} onClick={setValue} value={1}/>
+            <UncontrolledStar selected={value > 1} onClick={setValue} value={2}/>
+            <UncontrolledStar selected={value > 2} onClick={setValue} value={3}/>
+            <UncontrolledStar selected={value > 3} onClick={setValue} value={4}/>
+            <UncontrolledStar selected={value > 4} onClick={setValue} value={5}/>
         </div>
     );
 };
