9e23eccf02a04ecd54114af31e8a07a97fcb0b94
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-04 14:03:01 +0300

    refactor: deleted value props and modified onClick signature

--- src/common/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/common/components/UncontrolledRating/UncontrolledRating.tsx
@@ -7,11 +7,11 @@ export const UncontrolledRating = () => {
 
     return (
         <div>
-            <UncontrolledStar selected={value > 0} onClick={setValue} value={1}/>
-            <UncontrolledStar selected={value > 1} onClick={setValue} value={2}/>
-            <UncontrolledStar selected={value > 2} onClick={setValue} value={3}/>
-            <UncontrolledStar selected={value > 3} onClick={setValue} value={4}/>
-            <UncontrolledStar selected={value > 4} onClick={setValue} value={5}/>
+            <UncontrolledStar selected={value > 0} onClick={()=> setValue(1)}/>
+            <UncontrolledStar selected={value > 1} onClick={()=> setValue(2)}/>
+            <UncontrolledStar selected={value > 2} onClick={()=> setValue(3)}/>
+            <UncontrolledStar selected={value > 3} onClick={()=> setValue(4)}/>
+            <UncontrolledStar selected={value > 4} onClick={()=> setValue(5)}/>
         </div>
     );
 };
