4a6e4b88c3fc9ec6a3507f4f3ab56fa8f32e355c
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-04 13:40:43 +0300

    modified onClick signature

--- src/common/components/Star/Star.tsx
+++ src/common/components/Star/Star.tsx
@@ -2,8 +2,9 @@ import {StarProps} from "common/types/StarProps.ts";
 
 export const Star = (props: StarProps) => {
     console.log("Star rendering")
+
     return (
-        <span onClick={props.onClick}>
+        <span onClick={()=> props.onClick && props.onClick(props.value)}>
             {props.selected ? <b>Star </b> : "Star "}
         </span>
     )
