b46ec40e4887dee3d64a0d43f4e924a52a7169c7
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-07 01:49:40 +0300

    has been created DemonstrationRating component with useState function, for change state of component

--- src/common/stories/Rating/Rating.stories.tsx
+++ src/common/stories/Rating/Rating.stories.tsx
@@ -1,4 +1,6 @@
 import {Rating} from "common/components/Rating/Rating.tsx";
+import {useState} from "react";
+import {RatingValueProps} from "common/types/RatingValueProps.ts";
 import {action} from "@storybook/addon-actions";
 
 export default {
@@ -17,3 +19,12 @@ export const Rating2 = () => <Rating value={2} onClick={onClickHandler} />
 export const Rating3 = () => <Rating value={3} onClick={onClickHandler} />
 export const Rating4 = () => <Rating value={4} onClick={onClickHandler} />
 export const Rating5 = () => <Rating value={5} onClick={onClickHandler} />
+export const DemonstrationRating = () => {
+    const [rating, setRating] = useState<RatingValueProps>(0);
+
+    return (
+            <>
+                <Rating value={rating} onClick={setRating} />
+            </>
+        );
+}
