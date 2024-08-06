7a0bf28fcc0a7f43b5a0f9f1148ea2c74a567239
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-06 23:13:06 +0300

    has been created DemonstrationAccordion component with useState function, for change state of component

--- src/common/stories/Accordion.stories.tsx
+++ src/common/stories/Accordion.stories.tsx
@@ -1,5 +1,6 @@
 import {Meta} from "@storybook/react";
 import {Accordion} from "common/components/Accordion/Accordion.tsx";
+import {useState} from "react";
 
 const meta: Meta<typeof Accordion> = {
     title: 'Accordion',
@@ -15,3 +16,12 @@ export default meta;
 
 export const CollapsedAccordion = () => <Accordion title={"Collapsed Accordion"} collapsed={true} onClick={()=> {}} />
 export const OpenedAccordion = () => <Accordion title={"Opened Accordion"} collapsed={false} onClick={()=> {}}/>
+export const DemonstrationAccordion = () => {
+    const [collapsed, setCollapsed] = useState<boolean>(false);
+
+    return (
+        <>
+            <Accordion title={"Accordion"} collapsed={collapsed} onClick={setCollapsed}/>
+        </>
+    )
+}
