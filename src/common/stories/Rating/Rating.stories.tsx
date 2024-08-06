46f6b8efb8d6d78021fd16de1841ea6d6d11587c
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-07 01:46:09 +0300

    has been created Rating2 component, and set hardcode value of props, added callback action for ocClick

--- src/common/stories/Rating/Rating.stories.tsx
+++ src/common/stories/Rating/Rating.stories.tsx
@@ -13,3 +13,4 @@ export default {
 const onClickHandler = action("Clicked handler")
 export const EmptyRating = () => <Rating value={0} onClick={onClickHandler} />
 export const Rating1 = () => <Rating value={1} onClick={onClickHandler} />
+export const Rating2 = () => <Rating value={2} onClick={onClickHandler} />
